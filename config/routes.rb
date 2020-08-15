1bf45e10d272ff7de113985eab9d52d6fe084606
alexandros-cat <alexandros.cat@gmail.com>
2020-08-15 17:26:11 +0900

    チャット画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # こちらは削除してOKです
   root "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit,:update]
   resources :rooms, only: [:new, :create]
 end
